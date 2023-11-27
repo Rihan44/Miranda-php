089f82e771b5d544598bfef9a7a17e672b0647d7
Rihan44 <asmuela_bustamante@hotmail.es>
2023-11-27 18:23:55 +0100

    [Update] all done

--- rooms.php
+++ rooms.php
@@ -1,14 +1,35 @@
 <?php
 require_once('config.php');
 
-if(isset($_GET['check_in']) && isset($_GET['check_out'])) {
+$amenity_icons = [
+    'Bed Space' => '/img/bed-icon.png',
+    '24-Hour Guard' => '/img/gym-icon.png',
+    'Free Wifi' => '/img/wifi-icon.png',
+    'Air Conditioner' => '/img/cold-icon.png',
+    'Television' => '/img/bed-icon.png',
+    'Towels' => '/img/no-smoking-icon.png',
+    'Mini Bar' => '/img/cocktail-icon.png',
+    'Coffee Set' => '/img/bed-icon.png',
+    'Bathtub' => '/img/bed-icon.png',
+    'Jacuzzi' => '/img/bed-icon.png',
+    'Nice Views' => '/img/bed-icon.png',
+];
+
+
+if (isset($_GET['check_in']) && isset($_GET['check_out'])) {
     $check_in = htmlspecialchars($_GET['check_in']);
     $check_out = htmlspecialchars($_GET['check_out']);
 
-    $sql = "SELECT r.*, COALESCE(GROUP_CONCAT(DISTINCT rp.room_photo_url), 'https://tinyurl.com/RoomPhoto1') AS URL 
-        FROM rooms r
-        LEFT JOIN room_photos rp ON r.id = rp.room_id
-        LEFT JOIN bookings b ON r.id = b.room_id
+    $sql = "SELECT 
+            r.*, 
+            COALESCE(GROUP_CONCAT(DISTINCT rp.room_photo_url), 'https://tinyurl.com/RoomPhoto1') AS URL,
+            GROUP_CONCAT(DISTINCT a.amenity_name) AS amenities
+        FROM 
+            rooms r
+            LEFT JOIN room_photos rp ON r.id = rp.room_id
+            LEFT JOIN bookings b ON r.id = b.room_id
+            LEFT JOIN amenity_to_room atr ON r.id = atr.room_id
+            LEFT JOIN amenities a ON atr.amenity_id = a.id
         WHERE (
             ('$check_in' >= b.check_out OR '$check_out' <= b.check_in)
         )
@@ -30,17 +51,29 @@ if(isset($_GET['check_in']) && isset($_GET['check_out'])) {
     }
     
 } else {
-    
-    $sql = "SELECT r.*, COALESCE(GROUP_CONCAT(DISTINCT rp.room_photo_url), 'https://tinyurl.com/RoomPhoto1') AS URL FROM rooms r LEFT JOIN room_photos rp ON r.id = rp.room_id GROUP BY r.id";
+
+    $sql = "SELECT 
+            r.*, 
+            COALESCE(GROUP_CONCAT(DISTINCT rp.room_photo_url), 'https://tinyurl.com/RoomPhoto1') AS URL,
+            GROUP_CONCAT(DISTINCT a.amenity_name) AS amenities
+        FROM 
+            rooms r
+            LEFT JOIN room_photos rp ON r.id = rp.room_id
+            LEFT JOIN amenity_to_room atr ON r.id = atr.room_id
+            LEFT JOIN amenities a ON atr.amenity_id = a.id
+        GROUP BY r.id";
 
     $result = $conn->query($sql);
 
     $rooms = $result->fetch_all(MYSQLI_ASSOC);
 
+    foreach ($rooms as $room) {
+        $amenities_array = explode(',', $room['amenities']);
+    }
+
     $no_rooms = false;
 }
 
-echo $blade->run('rooms', ['rooms' => $rooms, 'no_rooms' => $no_rooms]);
+echo $blade->run('rooms', ['rooms' => $rooms, 'no_rooms' => $no_rooms, 'amenity_icons' => $amenity_icons, 'amenities_array' => $amenities_array]);
 
 $conn->close();
-?>
